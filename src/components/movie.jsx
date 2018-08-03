12b0b70720f8dcca3baba4f0731ac442a6ee6527
klendi <klendigocci@gmail.com>
2018-08-03 23:11:33 +0200

    Initial Commit

---
+++ src/components/movie.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Movie extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return;
+  }
+}
+
+export default Movie;
---
+++ src/services/moviesService.js
@@ -0,0 +1,31 @@
+async function searchMovie(title, callback) {
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${encodeURI(
+      title
+    )}`
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(myJson => {
+      callback(null, myJson);
+      return myJson;
+    });
+}
+
+function getGenresByID(id, callback) {
+  let data = [];
+
+  fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&language=en-US`
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(myJson => {
+      callback(null, myJson);
+      return myJson;
+    });
+}
+
+export default searchMovie;
