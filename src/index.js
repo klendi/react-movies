b93e7d064978afdc3098f4bbe2250f209b52b1a0
klendi <klendigocci@gmail.com>
2018-09-22 16:46:16 +0200

    Add History prop to router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles/style.css'
 import 'font-awesome/css/font-awesome.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import registerServiceWorker from './registerServiceWorker'
+const hist = createBrowserHistory()
+
 
 ReactDOM.render(
-  <Router>
+  <Router history={hist}>
     <App />
   </Router>,
   document.getElementById('root')
